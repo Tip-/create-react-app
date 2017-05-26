3d61f201c8b4ce578ee0d2d75f5204bcc45596cd
Henri Michel <henri@tymate.com>
2017-05-26 15:16:24 +0200

    .

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -153,7 +153,7 @@ module.exports = function(
 
   console.log(appPackage);
   console.log();
-  console.log(`Success! Created ${appName} at ${appPath}`);
+  console.log(`Successs! Created ${appName} at ${appPath}`);
   console.log('Inside that directory, you can run several commands:');
   console.log();
   console.log(chalk.cyan(`  ${displayedCommand} start`));
